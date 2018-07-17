10cc629343ed58c62d91d76edecd5c0c00086fdf
Suvan <suvanramani@gmail.com>
2018-07-17 15:50:36 -0700

    create Tree

--- 05BinaryTrees/BinaryTrees.playground/Contents.swift
+++ 05BinaryTrees/BinaryTrees.playground/Contents.swift
@@ -1,2 +1,18 @@
 import Foundation
 
+var tree: BinaryNode<Int> {
+    let zero = BinaryNode(value: 0)
+    let one = BinaryNode(value: 1)
+    let five = BinaryNode(value: 5)
+    let seven = BinaryNode(value: 7)
+    let eight = BinaryNode(value: 8)
+    let nine = BinaryNode(value: 9)
+    
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+    
+    return seven
+}
